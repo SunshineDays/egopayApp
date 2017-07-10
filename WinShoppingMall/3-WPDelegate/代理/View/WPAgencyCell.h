8ec400f57f450e1ce723d9836683fa79d769fb95
SunshineDays <sunshineday0619@163.com>
2017-07-10 13:34:56 +0800

    colectionView

---
+++ WinShoppingMall/3-WPDelegate/代理/View/WPAgencyCell.h
@@ -0,0 +1,13 @@
+//
+//  WPAgencyCell.h
+//  WinShoppingMall
+//
+//  Created by 易购付 on 2017/7/10.
+//  Copyright © 2017年 易购付. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPAgencyCell : UICollectionViewCell
+
+@end
---
+++ WinShoppingMall/3-WPDelegate/代理/View/WPAgencyCell.m
@@ -0,0 +1,18 @@
+//
+//  WPAgencyCell.m
+//  WinShoppingMall
+//
+//  Created by 易购付 on 2017/7/10.
+//  Copyright © 2017年 易购付. All rights reserved.
+//
+
+#import "WPAgencyCell.h"
+
+@implementation WPAgencyCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
